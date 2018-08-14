305ee5b7b5441d41c294b483c6346dbee0dadbea
manavdeep2ms <iit2016042@iiita.ac.in>
2018-08-14 07:07:38 +0530

    phase shift keying

---
+++ psk.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+int main(){
+	FILE* fp=fopen("psk.txt", "w");
+	int n, i, j;
+	scanf("%d", &n);
+	float a[n];
+	for(i=0; i<n; i++){
+		scanf("%f", &a[i]);
+	}
+	float b[100*n];
+	float old = 0;
+	for(i=0; i<n; i++){
+		if(a[i]==0){
+			for(j=100*i; j<(100*i)+100; j++){
+				b[j]=sin(old);
+                old += 0.1;
+			}
+		}
+		else if(a[i]==1){
+			for(j=100*i; j<(100*i)+100; j++){
+				b[j]=sin(old);
+				old -= 0.1;
+			}
+		}
+	}
+	for(i=0; i<100*n; i++){
+		fprintf(fp, "%d %f\n", i, b[i]);
+	}
+	fclose(fp);
+	return 0;
+}
