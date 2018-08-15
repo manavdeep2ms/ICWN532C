eb07ccfc35b3f7b9cde9f2a94e6261ab6c407892
manavdeep2ms <iit2016042@iiita.ac.in>
2018-08-16 03:44:15 +0530

    nrz

---
+++ nrz.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    FILE* fp=fopen("nrz.txt", "w");
+    int n, A;
+    scanf("%d%d", &n, &A);
+    int a[n], b[100*n];
+    int i, j;
+    for(i=0; i<n; i++){
+        scanf("%d", &a[i]);
+    }
+    for(i=0; i<n; i++){
+        if(a[i]==1){
+            for(j=100*i; j<(100*i)+100; j++){
+                b[j]=A;
+            }
+        }
+        else{
+            for(j=100*i; j<(100*i)+100; j++){
+                b[j]=0;
+            }
+        }
+    }
+    for(i=0; i<100*n; i++){
+		fprintf(fp, "%d %d\n", i, b[i]);
+	}
+	fclose(fp);
+	return 0;
+}
